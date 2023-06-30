13b04e8a4fd7e44763897538d1b1cd21ede58b35
Vlad <myborvlad@gmail.com>
2023-06-30 23:49:26 +0300

    Added Food details to the Cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FoodService} from "../services/food/food.service";
+import {Food} from "../shared/models/Food";
+import {StarRatingComponent} from "ng-starrating";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,19 @@ import {FoodService} from "../services/food/food.service";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
-  foods: String[] = []
+  foods: Food[] = []
   constructor(private foodService: FoodService) {}
 
   ngOnInit() {
     this.foods = this.foodService.getAll();
+  }
+
+  onRate($event: { oldValue: number; newValue: number; starRating: StarRatingComponent }) {
+    alert(
+      `Old Value:${$event.oldValue},
+      New Value: ${$event.newValue},
+      Checked Color: ${$event.starRating.checkedcolor},
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`
+    );
   }
 }
